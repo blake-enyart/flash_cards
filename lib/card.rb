fb0775464c34855eb704ef02f6abddcc8aa4edac
Blake Enyart <blake.enyart@gmail.com>
2019-01-23 17:00:53 -0700

    Tiny update to files

---
+++ lib/card.rb
@@ -0,0 +1,23 @@
+class Card
+  attr_accessor :question,
+                :answer,
+                :category
+
+   def initialize(question, answer, category)
+     @question = question
+     @answer = answer
+     @category = category
+   end
+
+   def question
+     return @question
+   end
+
+   def answer
+     return @answer
+   end
+
+   def category
+     return @category
+   end
+end
