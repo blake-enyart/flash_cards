38a1a0cc2bd123509f1a93fd20cf6b8cc0013db9
Blake Enyart <blake.enyart@gmail.com>
2019-01-25 08:48:38 -0700

    Developed cards method

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -8,21 +8,20 @@ require 'pry'
 class TurnTest < Minitest::Test
 
   def setup
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    @cards = [card_1, card_2, card_3]
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
   end
 
   def test_does_deck_exist
-    deck = Deck.new
 
-    assert_instance_of Deck, deck
+    assert_instance_of Deck, @deck
   end
 
   def test_it_has_cards_method
 
-    assert_equal @cards, deck.cards
-
+    assert_equal @cards, @deck.cards
   end
 end
