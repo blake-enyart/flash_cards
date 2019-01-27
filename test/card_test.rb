2bee6b2575a9f8fd3e4f0f659655f85c1c53e2d3
Blake Enyart <blake.enyart@gmail.com>
2019-01-27 15:03:58 -0700

    Refactor card files

--- test/card_test.rb
+++ test/card_test.rb
@@ -4,27 +4,30 @@ require './lib/card'
 
 class CardTest < Minitest::Test
 
-  def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  end
 
-    assert_instance_of Card, card
+  def test_it_exists
+
+    assert_instance_of Card, @card
   end
 
   def test_it_has_a_question
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    question = "What is the capital of Alaska?"
 
-    assert_equal "What is the capital of Alaska?", card.question
+    assert_equal question, @card.question
   end
 
   def test_it_has_an_answer
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    answer = "Juneau"
 
-    assert_equal "Juneau", card.answer
+    assert_equal answer, @card.answer
   end
 
   def test_it_has_a_category
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    category = :Geography
 
-    assert_equal :Geography, card.category
+    assert_equal category, @card.category
   end
 end
