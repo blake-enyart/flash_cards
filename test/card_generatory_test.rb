3e526b77f1e8495845d477aa7d6b7a74abc60148
Blake Enyart <blake.enyart@gmail.com>
2019-01-27 10:15:24 -0700

    Instantiate card_generator object

--- test/card_generatory_test.rb
+++ test/card_generatory_test.rb
@@ -4,15 +4,19 @@ require './lib/turn'
 require './lib/card'
 require './lib/deck'
 require './lib/round'
+require './lib/card_generator'
 require 'pry'
 
 class CardGeneratorTest < Minitest::Test
 
   def setup
+    filename = './lib/cards.txt'
+    @card_generator = CardGenerator.new(filename)
   end
 
   def test_does_card_generator_exist
 
     assert_instance_of CardGenerator, @card_generator
   end
+
 end
